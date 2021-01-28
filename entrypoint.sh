66e1d9214fd34c6211a5c9b8a184fb90f815f641
Lucas Medeiros Cardoso <clucasmedeiros@gmail.com>
2021-01-28 15:27:30 -0300

    chore: test ci script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 set -e
+github_token=$INPUT_GITHUB_TOKEN
+echo $github_token
+
+branch=$INPUT_BRANCH
+echo $branch
+
+tag=$INPUT_TAG
+echo $tag
 
 master_branch=$INPUT_MASTER_BRANCH
 echo $master_branch
@@ -7,5 +15,8 @@ echo $master_branch
 dev_branch=$INPUT_DEVELOPMENT_BRANCH
 echo $dev_branch
 
-echo "::set-output name=master_branch::$master_branch"
+echo "::set-output name=master_branch::$github_token"
+echo "::set-output name=development_branch::$branch"
+echo "::set-output name=master_branch::$tag"
 echo "::set-output name=development_branch::$dev_branch"
+echo "::set-output name=master_branch::$master_branch"
