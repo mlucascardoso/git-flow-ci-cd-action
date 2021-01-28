ea98ca0b6119791aecc8abdf0aff4de86fc2dfe6
Lucas Medeiros Cardoso <clucasmedeiros@gmail.com>
2021-01-28 16:01:40 -0300

    chore: test ci

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,22 +1,33 @@
 #!/bin/sh
 set -e
-github_token=$INPUT_GITHUB_TOKEN
-echo $github_token
 
 branch=$INPUT_CURRENT_BRANCH
-echo $branch
+feature_branch_prefix=$INPUT_FEATURE_BRANCH_PREFIX
+cuka="nao tem"
 
+case $feature_branch_prefix in
+           *"$branch"*)
+   cuka="its there"
+esac
+
+# feature
+# hotfix
+
+# if $branch contains 
+
+# feature/test-feature
+
+# git checkout develop
+# git merge --no-ff feature/MYFEATURE
+# git branch -d feature/MYFEATURE
+
+github_token=$INPUT_GITHUB_TOKEN
 tag=$INPUT_TAG
-echo $tag
-
 master_branch=$INPUT_MASTER_BRANCH
-echo $master_branch
-
 dev_branch=$INPUT_DEVELOPMENT_BRANCH
-echo $dev_branch
-
 echo "::set-output name=github_token::$github_token"
 echo "::set-output name=branch::$branch"
 echo "::set-output name=tag::$tag"
 echo "::set-output name=development_branch::$dev_branch"
 echo "::set-output name=master_branch::$master_branch"
+echo "::set-output name=cuka::$cuka"
