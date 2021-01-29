1de99d3efb16909df00ac434483b665f230c905b
Lucas Medeiros Cardoso <clucasmedeiros@gmail.com>
2021-01-29 15:56:59 -0300

    chore: test ci script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,40 +1,25 @@
 #!/bin/sh
 set -e
 
-branch=$INPUT_CURRENT_BRANCH
+github_token=$INPUT_GITHUB_TOKEN
+tag=$INPUT_TAG
+
+current_branch=$INPUT_CURRENT_BRANCH
+master_branch=$INPUT_MASTER_BRANCH
+dev_branch=$INPUT_DEVELOPMENT_BRANCH
 feature_branch_prefix=$INPUT_FEATURE_BRANCH_PREFIX
+bugfix_branch_prefix=$INPUT_BUGFIX_BRANCH_PREFIX
+release_branch_prefix=$INPUT_RELEASE_BRANCH_PREFIX
+hotfix_branch_prefix=$INPUT_HOTFIX_BRANCH_PREFIX
+support_branch_prefix=$INPUT_SUPPORT_BRANCH_PREFIX
+tag_prefix=$INPUT_TAG_BRANCH_PREFIX
 
-echo $feature_branch_prefix
-echo $branch
-
-if [[ -z "${branch##*$feature_branch_prefix*}" ]] ;then
-    echo "found"
-else
+# Feature finish
+if [[ -z "${current_branch##*$feature_branch_prefix*}" ]] ;then
+    # git checkout $dev_branch
+    # git merge --no-ff $current_branch
+    # git branch -d $current_branch
+elif [[ -z "${current_branch##*$bugfix_branch_prefix*}" ]] ;then
     echo "NOT found"
 fi
 
-# git checkout develop
-# git merge --no-ff feature/MYFEATURE
-#  	git branch -d feature/MYFEATURE
-
-# feature
-# hotfix
-
-# if $branch contains 
-
-# feature/test-feature
-
-# git checkout develop
-# git merge --no-ff feature/MYFEATURE
-# git branch -d feature/MYFEATURE
-
-github_token=$INPUT_GITHUB_TOKEN
-tag=$INPUT_TAG
-master_branch=$INPUT_MASTER_BRANCH
-dev_branch=$INPUT_DEVELOPMENT_BRANCH
-echo "::set-output name=github_token::$github_token"
-echo "::set-output name=branch::$branch"
-echo "::set-output name=tag::$tag"
-echo "::set-output name=development_branch::$dev_branch"
-echo "::set-output name=master_branch::$master_branch"
-echo "::set-output name=cuka::$cuka"
