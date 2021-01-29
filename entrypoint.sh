63cda1ae8f58d5c8aae591ff41f6e42e7e269677
Lucas Medeiros Cardoso <clucasmedeiros@gmail.com>
2021-01-29 16:18:39 -0300

    chore: test ci script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 set -e
 
-github_token=$INPUT_GITHUB_TOKEN
-tag=$INPUT_TAG
+# git credentials
+git config user.email ${INPUT_EMAIL}
+git config user.name ${INPUT_NAME}
+git config user.password ${INPUT_GITHUB_TOKEN}
 
 current_branch=$INPUT_CURRENT_BRANCH
 master_branch=$INPUT_MASTER_BRANCH
@@ -19,7 +21,7 @@ if [[ -z "${current_branch##*$feature_branch_prefix*}" ]] ;then
     echo "feature" 
     git checkout $dev_branch
     git merge --no-ff origin/$current_branch
-    git branch -d $current_branch
+    git branch -D $current_branch
 elif [[ -z "${current_branch##*$bugfix_branch_prefix*}" ]] ;then
     echo "bugfix"
 elif [[ -z "${current_branch##*$release_branch_prefix*}" ]] ;then
