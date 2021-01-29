2356120eb42e5ddf7b019fc2ee4b5b4e7d5eeaae
Lucas Medeiros Cardoso <clucasmedeiros@gmail.com>
2021-01-29 16:24:44 -0300

    chore: test ci script

--- entrypoint.sh
+++ entrypoint.sh
@@ -25,7 +25,8 @@ if [[ -z "${current_branch##*$feature_branch_prefix*}" ]] ;then
     echo "feature" 
     git checkout $dev_branch
     git merge --no-ff origin/$current_branch
-    git branch -D $current_branch
+    git branch -D origin/$current_branch
+    git push origin $dev_branch
 elif [[ -z "${current_branch##*$bugfix_branch_prefix*}" ]] ;then
     echo "bugfix"
 elif [[ -z "${current_branch##*$release_branch_prefix*}" ]] ;then
