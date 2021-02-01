46ab13f34c4913a948bc416988cd7c008be80342
Lucas Medeiros Cardoso <clucasmedeiros@gmail.com>
2021-02-01 14:00:15 -0300

    chore: test ci script

--- entrypoint.sh
+++ entrypoint.sh
@@ -31,8 +31,10 @@ remote_repo="https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/${GITHUB_R
 
 # Feature finish
 if [[ -z "${INPUT_CURRENT_BRANCH##*$INPUT_FEATURE_BRANCH_PREFIX*}" ]] ;then
+    git checkout ${INPUT_DEVELOPMENT_BRANCH}
+    git merge --no-ff origin/${INPUT_CURRENT_BRANCH}
     git push "${remote_repo}" HEAD:${INPUT_DEVELOPMENT_BRANCH} --follow-tags;
-    # git checkout ${INPUT_DEVELOPMENT_BRANCH}
+    
     # git merge --no-ff "${remote_repo}" HEAD:${INPUT_CURRENT_BRANCH}
     # git merge --no-ff origin/${INPUT_CURRENT_BRANCH}
     # git push --delete "${remote_repo}" HEAD:${INPUT_CURRENT_BRANCH}
