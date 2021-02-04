459658f3b7b27ecf439b6657893dc1c6c7d08737
Lucas Medeiros Cardoso <clucasmedeiros@gmail.com>
2021-02-04 09:20:50 -0300

    feat: create release handler

---
+++ src/modules/git-flow/handlers/release.ts
@@ -0,0 +1,50 @@
+import { CreateTagParams, GitFlowHandler } from '@/modules/git-flow/protocols';
+import { Branches, GitHub } from '@/infra/github/protocols';
+
+export class Release implements GitFlowHandler {
+    private readonly github: GitHub;
+
+    constructor(github: GitHub) {
+        this.github = github;
+    }
+
+    test(): boolean {
+        const branches = this.github.getBranches();
+        const prefixes = this.github.getPrefixes();
+        return branches.current.includes(prefixes.release);
+    }
+
+    public async handle(): Promise<string> {
+        const branches = this.github.getBranches();
+        const prefixes = this.github.getPrefixes();
+        const sha = await this.merge(branches);
+        await this.github.delete(branches.current);
+        await this.createTag({ branches, prefixes, sha });
+
+        return sha;
+    }
+
+    private async merge(branches: Branches): Promise<string> {
+        await this.github.merge(branches.current, branches.development);
+        const sha = await this.github.merge(branches.current, branches.main);
+
+        return sha;
+    }
+
+    private async createTag(params: CreateTagParams): Promise<void> {
+        const tag = this.getTagName(
+            params.branches.current,
+            params.prefixes.release,
+            params.prefixes.tag,
+        );
+
+        this.github.getCore().info(`SHA -------> ${params.sha}`);
+
+        await this.github.createTag(tag, params.sha);
+    }
+
+    private getTagName(currentBranch: string, releasePrefix: string, tagPrefix: string): string {
+        const branchName = currentBranch.split(releasePrefix).join('');
+        return `${tagPrefix}${branchName}`;
+    }
+}
