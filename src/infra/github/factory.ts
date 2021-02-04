a9441780115b392fcbbf098b22829936ad7098da
Lucas Medeiros Cardoso <clucasmedeiros@gmail.com>
2021-02-04 13:52:16 -0300

    refactor: removed extra line and add method accessor modifier that were missing in some methods

--- src/infra/github/factory.ts
+++ src/infra/github/factory.ts
@@ -3,7 +3,6 @@ import * as core from '@actions/core';
 import { GitHubService } from '@/infra/github/service';
 import { GitHub } from '@/infra/github/protocols';
 
-
 export class GitHubFactory {
     public static assemble(): GitHub {
         return new GitHubService(client, core);
